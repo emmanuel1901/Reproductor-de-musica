24040dd6222d5ced867107f44bd1ef13fe000de1
emmanuel1901 <emmanuel.montoya@udea.edu.co>
2025-07-29 15:45:51 -0500

    se actualizo

--- codigo/app.js
+++ codigo/app.js
@@ -1,23 +1,42 @@
-angular.module("Reproductor", [])
-    .controller("ControladorReproductor", function ($scope, $http) {
+angular.module("JuegoBingo", [])
+    .controller("ControladorJuegoBingo",
+        function ($scope) {
 
-        $scope.artistas = [];
-        $scope.canciones = [];
-        $scope.artistaSeleccionado = "";
-        $scope.cancionSeleccionada = "";
+            $scope.cantor = new Cantor();
+            $scope.jugadores = [];
+            $scope.juegoIniciado = false;
+            $scope.posicionGanador = -1;
 
-        //cargue de los datos (solo posible mediante servicio)
-        $http.get("datos/Musiteca.json")
-            .then(function (datos) {
-                $scope.artistas = datos.data.Artistas;
-            });
+            $scope.iniciarJuego = function () {
+                const totalJugadores = 7;
+                $scope.jugadores = [];
+                for (let i = 0; i < totalJugadores; i++) {
+                    $scope.jugadores.push({
+                        nombre: "Jugador " + (i + 1),
+                        tablaJugador: new Tabla(),
+                        ganador: false
+                    });
+                }
+                $scope.juegoIniciado = true;
+                $scope.cantor.iniciar();
+                $scope.posicionGanador = -1;
+            }
 
-        $scope.seleccionarArtista = function (artista) {
-            $scope.canciones = artista.Canciones;
-            $scope.artistaSeleccionado = artista.Nombre;
+            $scope.sacarBalota = function () {
+                $scope.cantor.sacarBalota();
+
+                for (i = 0; i < $scope.jugadores.length; i++) {
+                    $scope.jugadores[i].tablaJugador.taparNumero($scope.cantor.ultimaBalota);
+                    $scope.jugadores[i].ganador = $scope.jugadores[i].tablaJugador.verificarBingo();
+                    if ($scope.jugadores[i].ganador) {
+                        $scope.posicionGanador = i;
+                        $scope.juegoIniciado = false;
+                    }
+                }
+
+                if ($scope.cantor.pendientes == 0) {
+                    $scope.juegoIniciado = false;
+                }
+            }
         }
-
-        $scope.seleccionarCancion = function (cancion) {
-            $scope.cancionSeleccionada = cancion.Titulo + " - " + $scope.artistaSeleccionado;
-        }
-    });
+    );
