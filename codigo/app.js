0829907da6325fa3988296c19405f4b989b1d8d4
emmanuel1901 <emmanuel.montoya@udea.edu.co>
2025-07-29 15:28:16 -0500

    acabé de llegar a clase

--- codigo/app.js
+++ codigo/app.js
@@ -1,12 +1,23 @@
 angular.module("Reproductor", [])
     .controller("ControladorReproductor", function ($scope, $http) {
 
-
         $scope.artistas = [];
+        $scope.canciones = [];
+        $scope.artistaSeleccionado = "";
+        $scope.cancionSeleccionada = "";
 
         //cargue de los datos (solo posible mediante servicio)
         $http.get("datos/Musiteca.json")
             .then(function (datos) {
                 $scope.artistas = datos.data.Artistas;
             });
+
+        $scope.seleccionarArtista = function (artista) {
+            $scope.canciones = artista.Canciones;
+            $scope.artistaSeleccionado = artista.Nombre;
+        }
+
+        $scope.seleccionarCancion = function (cancion) {
+            $scope.cancionSeleccionada = cancion.Titulo + " - " + $scope.artistaSeleccionado;
+        }
     });
